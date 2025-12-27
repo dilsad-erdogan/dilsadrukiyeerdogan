9f6741097d785c29d8c009d6b9802497db47a32a
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-12-27 14:41:34 +0300

    publish

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/dilsadrukiyeerdogan/",   // repo adı neyse o
+  plugins: [react()],
+});
